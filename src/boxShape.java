368da5de43de35281cea4253b4d4bead0505050e
swilliam01 <sehrishwilliam@gmail.com>
2018-10-04 10:43:23 -0400

    Initial commit

---
+++ src/boxShape.java
@@ -0,0 +1,20 @@
+import static java.lang.System.*;
+
+public class boxShape {
+
+
+    public static void main(String[] args) {
+        out.println("*********");
+        out.println("*       *");
+        out.println("*       *");
+        out.println("*       *");
+        out.println("*       *");
+        out.println("*********");
+
+
+    }
+
+
+}
+
+
